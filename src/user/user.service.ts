71a040efdf938cfa1679f3d55b5421664d75e22c
paekung <supakit.3t@gmail.com>
2026-03-02 09:59:39 +0700

    p03

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.interface';
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const filePath = join(__dirname, '../../data/users.json')
+
 
 @Injectable()
 export class UserService {
   test() {
     return [];
+  }
+
+  findAll() : IUser{
+    const readUserData = readFileSync(filePath, 'utf-8')
+    const userData: IUser = JSON.parse(readUserData)
+    return userData
   }
 }
